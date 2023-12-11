8c7a1475ce614498602efffeeea7d65c2bf63c57
Savini Pietro <savinipietro@gmail.com>
2023-12-11 16:11:57 +0100

    add comment

--- src/components/Widget/Widget.tsx
+++ src/components/Widget/Widget.tsx
@@ -70,58 +70,58 @@ export const Widget = (props:props,) => {
     
     
 
+    //da fare un singolo condizionale per variabile isLoading creando un componente widget-skeleton
+    return (
+        <>
+            <Box component='div' minWidth={244} className='widget'>
+            
+                <Link to={data?.to ? data?.to : '/'}>
+                    <Card sx={{minWidth:244}}>
+                        <CardContent>
+                            <Box component='div' className="widget-header">
+                                {isLoading ? (
+                                    <>
+                                        <Skeleton animation='wave' variant="circular" width={40} height={40} sx={{marginRight:"1rem"}}/>  
+                                        <Skeleton animation='wave' width={100} height={20}/>
+                                    </>
+                                ):(
+                                <>
+                                        {data?.image ? data?.image : <Icon sx={{fontSize:'4rem'}}>{data?.icon ? data?.icon : 'all_inbox'}</Icon>}
+                                        <Typography  component='h6' fontSize={settings?.titleSize ? settings.titleSize : '1rem'} color={settings?.titleColor ? settings?.titleColor : '#877f87ff'} >{data?.title ? data.title : 'Titolo del Widget' }</Typography>
+                                </>
 
-  return (
-    <>
-        <Box component='div' minWidth={244} className='widget'>
-           
-            <Link to={data?.to ? data?.to : '/'}>
-                <Card sx={{minWidth:244}}>
-                    <CardContent>
-                        <Box component='div' className="widget-header">
-                            {isLoading ? (
-                                <>
-                                    <Skeleton animation='wave' variant="circular" width={40} height={40} sx={{marginRight:"1rem"}}/>  
-                                    <Skeleton animation='wave' width={100} height={20}/>
-                                </>
-                            ):(
-                               <>
-                                    {data?.image ? data?.image : <Icon sx={{fontSize:'4rem'}}>{data?.icon ? data?.icon : 'all_inbox'}</Icon>}
-                                    <Typography  component='h6' fontSize={settings?.titleSize ? settings.titleSize : '1rem'} color={settings?.titleColor ? settings?.titleColor : '#877f87ff'} >{data?.title ? data.title : 'Titolo del Widget' }</Typography>
-                               </>
+                                )}
+                            </Box>
+                            <Box component='div' className="widget-body">
+                                {isLoading ? (
+                                    <Skeleton animation='wave' sx={{marginBottom:'.5rem'}} variant='rectangular' width='100%' height={40}/>
+                                ):(
+                                    <>
+                                        {data?.body && data.body.map((item, index)=> (
+                                            <Box key={`${item}-${item.text}-${index}`} component='div' className='widget-content'>
+                                                <Typography sx={{marginRight:'.3rem'}} component='h3' color={settings?.bodyColor ? settings.bodyColor : 'black'} fontSize={settings?.numberSize ? settings.numberSize : '2rem'} fontWeight={600}> {item.counter } </Typography>
+                                                <Typography component={'p'} sx={{marginTop:'.5rem'}} color={settings?.bodyColor ? settings.bodyColor : 'black'} fontSize={settings?.subtitleSize ? settings.subtitleSize : '1rem' } fontWeight={600}> {item.text } </Typography>
+                                            </Box>
+                                        ))}
+                                    </>
+                                )}
+                            </Box>
+                            <Box component='div' className='widget-link'>
+                                {isLoading? (
+                                    <>
+                                        <Skeleton animation='wave' width={100} height={20}/>
+                                    </>
+                                ):(
+                                    <>
+                                        <Typography component='p' variant='button' color={settings?.linkColor ? settings.linkColor : '#53a5cfff'} fontSize={settings?.linkSize ? settings.linkSize : '.7rem'}>{data?.linkText ? data.linkText : `vai a gestione ${data?.title}`}</Typography>
+                                    </>
+                                )}
+                            </Box>
+                        </CardContent>  
+                    </Card>
+                </Link> 
+            </Box>
+        </>
 
-                            )}
-                        </Box>
-                        <Box component='div' className="widget-body">
-                            {isLoading ? (
-                                <Skeleton animation='wave' sx={{marginBottom:'.5rem'}} variant='rectangular' width='100%' height={40}/>
-                            ):(
-                                <>
-                                    {data?.body && data.body.map((item, index)=> (
-                                        <Box key={`${item}-${item.text}-${index}`} component='div' className='widget-content'>
-                                            <Typography sx={{marginRight:'.3rem'}} component='h3' color={settings?.bodyColor ? settings.bodyColor : 'black'} fontSize={settings?.numberSize ? settings.numberSize : '2rem'} fontWeight={600}> {item.counter } </Typography>
-                                            <Typography component={'p'} sx={{marginTop:'.5rem'}} color={settings?.bodyColor ? settings.bodyColor : 'black'} fontSize={settings?.subtitleSize ? settings.subtitleSize : '1rem' } fontWeight={600}> {item.text } </Typography>
-                                        </Box>
-                                    ))}
-                                </>
-                            )}
-                        </Box>
-                        <Box component='div' className='widget-link'>
-                            {isLoading? (
-                                <>
-                                    <Skeleton animation='wave' width={100} height={20}/>
-                                </>
-                            ):(
-                                <>
-                                    <Typography component='p' variant='button' color={settings?.linkColor ? settings.linkColor : '#53a5cfff'} fontSize={settings?.linkSize ? settings.linkSize : '.7rem'}>{data?.linkText ? data.linkText : `vai a gestione ${data?.title}`}</Typography>
-                                </>
-                            )}
-                        </Box>
-                    </CardContent>  
-                </Card>
-            </Link> 
-        </Box>
-    </>
-
-  )
+    )
 }
